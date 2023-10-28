4c00ec9339e218bf1ad3159fd481603bd46159b6
moraes.jonathan <moraes.jonathan@mj.gov.br>
2023-10-28 00:45:54 -0300

    fix - removing debugger

--- src/app/exception/error-interceptor.ts
+++ src/app/exception/error-interceptor.ts
@@ -7,20 +7,19 @@ import {ModalService} from "../shared/component/modal/modal.service";
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(
-        private modalService: ModalService
-    ) {
-    }
+  constructor(
+    private modalService: ModalService
+  ) {
+  }
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(
-            catchError((error: HttpErrorResponse) => {
-                debugger;
-                this.modalService.showMessage(error.error.message)
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.modalService.showMessage(error.error.message)
 
-                return throwError(new Error(error.error));
-            })
-        );
-    }
+        return throwError(new Error(error.error));
+      })
+    );
+  }
 
 }
